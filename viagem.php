d500a627e1633f5509db02c1b79c9557f0829094
MaiaC14 <48133608+MaiaC14@users.noreply.github.com>
2019-03-24 17:33:28 -0300

    Update viagem.php

--- viagem.php
+++ viagem.php
@@ -1,11 +1,11 @@
 <?php
 
 print "Qual é a distância a ser percorrida?"
-$Distancia = (int) fgets (STDIN); // em km
+$distancia = (int) fgets (STDIN); // em km
 
 print "A que velocidade você pretende percorrer essa distância?"
 $velocidade_media = (int) fgets (STDIN); // em km/h
 
-$Tempo = $Distancia/$velocidade_media;
+$Tempo = $distancia/$velocidade_media;
 
-print "O tempo estimado da viagem deve ser de $Tempo hora(s)";
+print "O tempo estimado da viagem deve ser de $Tempo hora(s).";
