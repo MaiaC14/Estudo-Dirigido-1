ee6866c4be7dfb360be23525e97459072ef2cf20
MaiaC14 <48133608+MaiaC14@users.noreply.github.com>
2019-03-24 17:28:15 -0300

    Update tempo.php

--- tempo.php
+++ tempo.php
@@ -2,16 +2,13 @@
 
    print "Informe o número de dias:";
    $dias =  (int) fgets (STDIN);
-   // multiplicar por 86400
+   
    $horas = $dias * 24;
-   // multiplicar por 3600
+  
    $minutos = $horas * 60;
-   // multiplicar por 60
+   
    $segundos = $minutos * 60;
-   // multiplicar por 60
-
 
    $total_segundos = $segundos + $minutos + $horas + $dias;
 
    print "A soma de todos os segundos é de $segundos";
-
