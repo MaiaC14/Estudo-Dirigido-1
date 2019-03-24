c4ca98b0401512f7a2945618de8f1fc45f76637a
MaiaC14 <48133608+MaiaC14@users.noreply.github.com>
2019-03-24 17:19:30 -0300

    Update aluguel.php

--- aluguel.php
+++ aluguel.php
@@ -6,9 +6,9 @@ $dias_com_carro = (int) fgets (STDIN); // R$60,00 cada dia
 print "Quantos km foram percorridos?";
 $km_corrido = (int) fgets (STDIN); // R$0,15 cada km
 
-$preco_dias = $diasComCarro *60; 
+$preco_dias = $diasComCarro * 60; 
 
-$preco_Km = $kmCorrido *0,15;
+$preco_Km = $kmCorrido * 0,15;
 
 $preco_total = $preco_dias + $preco_km
 
