505a8a04ec62b0e0bdee7ea7d3ee1522adabe514
MaiaC14 <48133608+MaiaC14@users.noreply.github.com>
2019-03-24 17:22:32 -0300

    Update desconto.php

--- desconto.php
+++ desconto.php
@@ -9,4 +9,5 @@ $porcentagem_desconto = (int) fgets (STDIN);
 $preco_desconto = $preco*(1-($porcentagem_desconto/100));
 
 $valor_desconto = $preco - $preco_desconto;
-echo "o valor do desconto é de $valor_desconto . O novo valor do produto é de $preco_desconto";
+
+print "O valor do desconto é de $valor_desconto . O novo valor do produto é de $preco_desconto";
